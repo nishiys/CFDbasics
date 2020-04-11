5dc815f0360d6e75d37bd4f7e0c4df3f9673e39d
Yasunari Nishi <yssubnr.out@gmail.com>
2020-04-12 00:03:21 +0200

    CellQuad4 modified

--- 03_heatDiffuion2d/mesh/inc/CellQuad4.hpp
+++ 03_heatDiffuion2d/mesh/inc/CellQuad4.hpp
@@ -17,56 +17,72 @@ public:
     ~CellQuad4();
 
     inline unsigned int GetID() const { return id_; };
-    inline Node2d* GetNode1() const { return pNode1_; };
-    inline Node2d* GetNode2() const { return pNode2_; };
-    inline Node2d* GetNode3() const { return pNode3_; };
-    inline Node2d* GetNode4() const { return pNode4_; };
 
-    inline Face2d* Face1() { return &face1_; };
-    inline Face2d* Face2() { return &face2_; };
-    inline Face2d* Face3() { return &face3_; };
-    inline Face2d* Face4() { return &face4_; };
+    inline Node2d* GetNode(unsigned int index) const { return pNodes_[index]; };
+    // inline Node2d* GetNode1() const { return pNode1_; };
+    // inline Node2d* GetNode2() const { return pNode2_; };
+    // inline Node2d* GetNode3() const { return pNode3_; };
+    // inline Node2d* GetNode4() const { return pNode4_; };
 
-    inline void SetNeighbor1Ptr(CellQuad4* pCellQuad)
+    inline Face2d* Face(unsigned int index) { return &faces_[index]; };
+    // inline Face2d* Face1() { return &face1_; };
+    // inline Face2d* Face2() { return &face2_; };
+    // inline Face2d* Face3() { return &face3_; };
+    // inline Face2d* Face4() { return &face4_; };
+
+    inline void SetNeighborPtr(unsigned int index, CellQuad4* pCellQuad)
     {
-        pNeighbor1_ = pCellQuad;
-    };
-    inline void SetNeighbor2Ptr(CellQuad4* pCellQuad)
-    {
-        pNeighbor2_ = pCellQuad;
-    };
-    inline void SetNeighbor3Ptr(CellQuad4* pCellQuad)
-    {
-        pNeighbor3_ = pCellQuad;
-    };
-    inline void SetNeighbor4Ptr(CellQuad4* pCellQuad)
-    {
-        pNeighbor4_ = pCellQuad;
+        pNeighbors_[index] = pCellQuad;
     };
 
-    inline CellQuad4* GetNeighbors1Ptr() const { return pNeighbor1_; };
-    inline CellQuad4* GetNeighbors2Ptr() const { return pNeighbor2_; };
-    inline CellQuad4* GetNeighbors3Ptr() const { return pNeighbor3_; };
-    inline CellQuad4* GetNeighbors4Ptr() const { return pNeighbor4_; };
+    // inline void SetNeighbor1Ptr(CellQuad4* pCellQuad)
+    // {
+    //     pNeighbor1_ = pCellQuad;
+    // };
+    // inline void SetNeighbor2Ptr(CellQuad4* pCellQuad)
+    // {
+    //     pNeighbor2_ = pCellQuad;
+    // };
+    // inline void SetNeighbor3Ptr(CellQuad4* pCellQuad)
+    // {
+    //     pNeighbor3_ = pCellQuad;
+    // };
+    // inline void SetNeighbor4Ptr(CellQuad4* pCellQuad)
+    // {
+    //     pNeighbor4_ = pCellQuad;
+    // };
+
+    inline CellQuad4* GetNeighborPtr(unsigned int index) const
+    {
+        return pNeighbors_[index];
+    };
+    // inline CellQuad4* GetNeighbors2Ptr() const { return pNeighbor2_; };t
+    // inline CellQuad4* GetNeighbors1Ptr() const { return pNeighbor1_; };
+    // inline CellQuad4* GetNeighbors2Ptr() const { return pNeighbor2_; };
+    // inline CellQuad4* GetNeighbors3Ptr() const { return pNeighbor3_; };
+    // inline CellQuad4* GetNeighbors4Ptr() const { return pNeighbor4_; };
 
     Variable var;
 
 private:
     unsigned int id_;
-    Node2d* pNode1_;
-    Node2d* pNode2_;
-    Node2d* pNode3_;
-    Node2d* pNode4_;
+    std::array<Node2d*, 4> pNodes_;
+    // Node2d* pNode1_;
+    // Node2d* pNode2_;
+    // Node2d* pNode3_;
+    // Node2d* pNode4_;
 
-    Face2d face1_;
-    Face2d face2_;
-    Face2d face3_;
-    Face2d face4_;
+    std::array<Face2d, 4> faces_;
+    // Face2d face1_;
+    // Face2d face2_;
+    // Face2d face3_;
+    // Face2d face4_;
 
-    CellQuad4* pNeighbor1_;
-    CellQuad4* pNeighbor2_;
-    CellQuad4* pNeighbor3_;
-    CellQuad4* pNeighbor4_;
+    std::array<CellQuad4*, 4> pNeighbors_;
+    // CellQuad4* pNeighbor1_;
+    // CellQuad4* pNeighbor2_;
+    // CellQuad4* pNeighbor3_;
+    // CellQuad4* pNeighbor4_;
 
     void CheckFaceNormals();
     void CalcCentroid();
--- 03_heatDiffuion2d/mesh/src/CellQuad4.cpp
+++ 03_heatDiffuion2d/mesh/src/CellQuad4.cpp
@@ -5,21 +5,18 @@
 CellQuad4::CellQuad4() {}
 CellQuad4::CellQuad4(unsigned int id, Node2d* pNode1, Node2d* pNode2,
                      Node2d* pNode3, Node2d* pNode4)
-    : pNode1_(pNode1),
-      pNode2_(pNode2),
-      pNode3_(pNode3),
-      pNode4_(pNode4),
+    : pNodes_{pNode1, pNode2, pNode3, pNode4},
       id_(id),
-      face1_(pNode1_, pNode2_),
-      face2_(pNode2_, pNode3_),
-      face3_(pNode3_, pNode4_),
-      face4_(pNode4_, pNode1_),
       centroid_(2),
-      pNeighbor1_(nullptr),
-      pNeighbor2_(nullptr),
-      pNeighbor3_(nullptr),
-      pNeighbor4_(nullptr)
+      pNeighbors_{nullptr, nullptr, nullptr, nullptr}
 {
+    Face2d face1(pNode1, pNode2);
+    Face2d face2(pNode2, pNode3);
+    Face2d face3(pNode3, pNode4);
+    Face2d face4(pNode4, pNode1);
+    faces_ = {face1, face2, face3, face4};
+
+    /*--- Calculate fundamental values ---*/
     CalcCentroid();
     CheckFaceNormals();
     CalcVolume();
@@ -29,40 +26,30 @@ CellQuad4::~CellQuad4() {}
 
 void CellQuad4::CheckFaceNormals()
 {
-    Eigen::VectorXd cellcent_to_face1cent(2);
-    Eigen::VectorXd cellcent_to_face2cent(2);
-    Eigen::VectorXd cellcent_to_face3cent(2);
-    Eigen::VectorXd cellcent_to_face4cent(2);
+    std::array<Eigen::Vector2d, 4> cellcent_to_facecent;
 
-    cellcent_to_face1cent = face1_.GetFaceCenter() - centroid_;
-    cellcent_to_face2cent = face2_.GetFaceCenter() - centroid_;
-    cellcent_to_face3cent = face3_.GetFaceCenter() - centroid_;
-    cellcent_to_face4cent = face4_.GetFaceCenter() - centroid_;
-
-    if (cellcent_to_face1cent.dot(face1_.GetNormalVec()) < 0)
+    for (size_t iFace = 0; iFace < faces_.size(); ++iFace)
     {
-        face1_.FlipNormalVec();
-    }
-    if (cellcent_to_face2cent.dot(face2_.GetNormalVec()) < 0)
-    {
-        face2_.FlipNormalVec();
-    }
-    if (cellcent_to_face3cent.dot(face3_.GetNormalVec()) < 0)
-    {
-        face3_.FlipNormalVec();
-    }
-    if (cellcent_to_face4cent.dot(face4_.GetNormalVec()) < 0)
-    {
-        face4_.FlipNormalVec();
+        cellcent_to_facecent[iFace] = faces_[iFace].GetFaceCenter() - centroid_;
+        if (cellcent_to_facecent[iFace].dot(faces_[iFace].GetNormalVec()) < 0)
+        {
+            faces_[iFace].FlipNormalVec();
+        }
     }
 }
 
 void CellQuad4::CalcCentroid()
 {
-    centroid_(0) = 0.25 * (pNode1_->GetX() + pNode2_->GetX() + pNode3_->GetX() +
-                           pNode4_->GetX());
-    centroid_(1) = 0.25 * (pNode1_->GetY() + pNode2_->GetY() + pNode3_->GetY() +
-                           pNode4_->GetY());
+    unsigned int nNode = pNodes_.size();
+    centroid_(0)       = 0;
+    centroid_(1)       = 0;
+    for (unsigned int iNode = 0; iNode < nNode; ++iNode)
+    {
+        centroid_(0) +=
+            1.0 / static_cast<double>(nNode) * pNodes_[iNode]->GetX();
+        centroid_(1) +=
+            1.0 / static_cast<double>(nNode) * pNodes_[iNode]->GetY();
+    }
 
     std::cout << "Cell " << id_ << " centroid: (" << centroid_(0) << ", "
               << centroid_(1) << ")" << std::endl;
@@ -71,10 +58,13 @@ void CellQuad4::CalcCentroid()
 void CellQuad4::CalcVolume()
 {
     // using Gauss divergence theorem
-    volume_ = 0.5 * face1_.GetNormalVec().dot(face1_.GetFaceCenter());
-    volume_ += 0.5 * face2_.GetNormalVec().dot(face2_.GetFaceCenter());
-    volume_ += 0.5 * face3_.GetNormalVec().dot(face3_.GetFaceCenter());
-    volume_ += 0.5 * face4_.GetNormalVec().dot(face4_.GetFaceCenter());
-
+    unsigned int nDim = 2;
+    volume_           = 0;
+    for (unsigned int iFace = 0; iFace < faces_.size(); ++iFace)
+    {
+        volume_ +=
+            1.0 / static_cast<double>(nDim) *
+            faces_[iFace].GetNormalVec().dot(faces_[iFace].GetFaceCenter());
+    }
     std::cout << "Cell " << id_ << " volume: " << volume_ << std::endl;
 }
